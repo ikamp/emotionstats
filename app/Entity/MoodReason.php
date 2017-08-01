52ceb4b0821f5d8e08a07dbec4eafd2b59b4d8db
Erbil Silik <erbil.silik@yandex.com>
2017-08-01 16:50:59 +0300

    Added getters and setters [BE-4]

--- app/Entity/MoodReason.php
+++ app/Entity/MoodReason.php
@@ -8,4 +8,68 @@ class MoodReason
     protected $employee_id;
     protected $mood_id;
     protected $reason;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmployeeId()
+    {
+        return $this->employee_id;
+    }
+
+    /**
+     * @param mixed $employee_id
+     */
+    public function setEmployeeId($employee_id)
+    {
+        $this->employee_id = $employee_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMoodId()
+    {
+        return $this->mood_id;
+    }
+
+    /**
+     * @param mixed $mood_id
+     */
+    public function setMoodId($mood_id)
+    {
+        $this->mood_id = $mood_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getReason()
+    {
+        return $this->reason;
+    }
+
+    /**
+     * @param mixed $reason
+     */
+    public function setReason($reason)
+    {
+        $this->reason = $reason;
+    }
 }
