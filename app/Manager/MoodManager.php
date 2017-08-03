d4d6ebf0ffc4f7061e0084d2c3425a8130651a2e
Eren Akpınar <erenakpinar123@gmail.com>
2017-08-03 20:54:33 +0300

    Average mood calculated. (BE-11)

--- app/Manager/MoodManager.php
+++ app/Manager/MoodManager.php
@@ -17,42 +17,65 @@ class MoodManager
         $moods = MoodModel::where('employee_id', Auth::id())
             ->get()
             ->groupBy('mood');
-        if (isset($moods['1'])) {
-            $mood['1Total'] = count($moods['1']);
-        }
-        if (isset($moods['2'])) {
-            $mood['2Total'] = count($moods['2']);
-        }
-        if (isset($moods['3'])) {
-            $mood['3Total'] = count($moods['3']);
-        }
-        if (isset($moods['4'])) {
-            $mood['4Total'] = count($moods['4']);
-        }
-        if (isset($moods['5'])) {
-            $mood['5Total'] = count($moods['5']);
-        }
 
-        $mood['totalCount'] = count($moods);
+        $mood = [];
+        if (isset($moods)) {
+            $mood['totalCount'] = 0;
+            $mood['average'] = 0;
 
-        $singleValue = 100 / $mood['totalCount'];
+            if (isset($moods['1'])) {
+                $mood['1Total'] = count($moods['1']);
+                $mood['totalCount'] += $mood['1Total'];
+                $mood['average'] += $mood['1Total'];
+            }
 
-        if (isset($moods['1'])) {
-            $mood['1TotalPercent'] = count($moods['1']) * $singleValue;
-        }
-        if (isset($moods['2'])) {
-            $mood['2TotalPercent'] = count($moods['2']) * $singleValue;
-        }
-        if (isset($moods['3'])) {
-            $mood['3TotalPercent'] = count($moods['3']) * $singleValue;
-        }
-        if (isset($moods['4'])) {
-            $mood['4TotalPercent'] = count($moods['4']) * $singleValue;
-        }
-        if (isset($moods['5'])) {
-            $mood['5TotalPercent'] = count($moods['5']) * $singleValue;
-        }
+            if (isset($moods['2'])) {
+                $mood['2Total'] = count($moods['2']);
+                $mood['totalCount'] += $mood['2Total'];
+                $mood['average'] += $mood['2Total'] * 2;
+            }
 
+            if (isset($moods['3'])) {
+                $mood['3Total'] = count($moods['3']);
+                $mood['totalCount'] += $mood['3Total'];
+                $mood['average'] += $mood['3Total'] * 3;
+            }
+
+            if (isset($moods['4'])) {
+                $mood['4Total'] = count($moods['4']);
+                $mood['totalCount'] += $mood['4Total'];
+                $mood['average'] += $mood['4Total'] * 4;
+            }
+
+            if (isset($moods['5'])) {
+                $mood['5Total'] = count($moods['5']);
+                $mood['totalCount'] += $mood['5Total'];
+                $mood['average'] += $mood['5Total'] * 5;
+            }
+
+            $mood['average'] = round($mood['average'] / $mood['totalCount']);
+            $singleMoodValue = 100 / $mood['totalCount'];
+
+            if (isset($moods['1'])) {
+                $mood['1TotalPresent'] = number_format($singleMoodValue * $mood['1Total'], 1);
+            }
+
+            if (isset($moods['2'])) {
+                $mood['2TotalPresent'] = number_format($singleMoodValue * $mood['2Total'], 1);
+            }
+
+            if (isset($moods['3'])) {
+                $mood['3TotalPresent'] = number_format($singleMoodValue * $mood['3Total'], 1);
+            }
+
+            if (isset($moods['4'])) {
+                $mood['4TotalPresent'] = number_format($singleMoodValue * $mood['4Total'], 1);
+            }
+
+            if (isset($moods['5'])) {
+                $mood['5TotalPresent'] = number_format($singleMoodValue * $mood['5Total'], 1);
+            }
+        }
 
         return $mood;
     }
