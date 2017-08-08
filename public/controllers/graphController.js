fbde8178849eab775e65643de9d1136b24ed2c86
Erbil Silik <erbil.silik@yandex.com>
2017-08-08 10:40:31 +0300

    Distribution chart is added [HK-7]

--- public/controllers/graphController.js
+++ public/controllers/graphController.js
@@ -1,16 +1,89 @@
 angular.module('emotionStatsApp')
-    .controller('GraphController', ["$scope", function ($scope, $rootScope) {
-        $scope.myDataSource1 = {
-            chart: {caption: "Some week"},
-            data: [
-                {label: "Saturday", value: "100"},
-                {label: "Sunday", value: "300"},
-                {label: "Monday", value: "150"},
-                {label: "Tuesday", value: "240"},
-                {label: "Wednesday", value: "300"},
-                {label: "Thursday", value: "90"},
-                {label: "Friday", value: "170"}
-            ]
+    .controller('GraphController', graphController);
+
+function graphController($scope, DataService) {
+    $scope.myDataSource1 = {};
+    DataService.getMood(function (response) {
+        $scope.data= response;
+        $scope.sad = {
+            "rate": 0,
+            "total": 0
+        };
+        $scope.unhappy = {
+            "rate": 0,
+            "total": 0
+        };
+        $scope.happy = {
+            "rate": 0,
+            "total": 0
+        };
+        $scope.ok = {
+            "rate": 0,
+            "total": 0
+        };
+        $scope.satisfied = {
+            "rate": 0,
+            "total": 0
         };
 
-    }]);
+        if (response.average == 1) {
+            $scope.mood = "sad"
+        } else if (response.average == 2) {
+            $scope.mood = "unhappy"
+        } else if (response.average == 3) {
+            $scope.mood = "ok"
+        } else if (response.average == 4) {
+            $scope.mood = "satisfied"
+        } else if (response.average == 5) {
+            $scope.mood = "happy"
+        }
+
+        if (response.TotalPercent1) {
+            $scope.sad = {
+                "rate": response.TotalPercent1.toString(),
+                "total": response.Total1
+            };
+        }
+        if (response.TotalPercent2) {
+            $scope.unhappy = {
+                "rate": response.TotalPercent2.toString(),
+                "total": response.Total2
+            };
+        }
+        if (response.TotalPercent3) {
+            $scope.ok = {
+                "rate": response.TotalPercent3.toString(),
+                "total": response.Total3
+            };
+        }
+        if (response.TotalPercent4) {
+            $scope.satisfied = {
+                "rate": response.TotalPercent4.toString(),
+                "total": response.Total4
+            };
+        }
+        if (response.TotalPercent5) {
+            $scope.happy = {
+                "rate": response.TotalPercent5.toString(),
+                "total": response.Total5
+            };
+        }
+
+        $scope.myDataSource1 = {
+            chart: {caption: "Your mood"},
+            data: [
+                {label: "Sad", value: $scope.sad.rate},
+                {label: "Unhappy", value: $scope.unhappy.rate},
+                {label: "Ok", value: $scope.ok.rate},
+                {label: "Satisfied", value: $scope.satisfied.rate},
+                {label: "Happy", value: $scope.happy.rate}
+            ]
+
+        };
+    });
+
+
+
+
+
+}
