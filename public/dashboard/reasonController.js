6a42afa19f9c9aa9c6578a7251fbec3b8ea9a91b
anakin <dilara.gzbyk19@gmail.com>
2017-08-11 03:29:10 +0300

    Edited top 5 reason files.

--- public/dashboard/reasonController.js
+++ public/dashboard/reasonController.js
@@ -160,14 +160,12 @@ function reasonController($rootScope, $scope, DataService) {
 
     $scope.bubbleSort = function (items) {
         var length = items.length;
-        for (var i = 0; i < length; i++) { //Number of passes
-            for (var j = 0; j < (length - i - 1); j++) { //Notice that j < (length - i)
-                //Compare the adjacent positions
+        for (var i = 0; i < length; i++) {
+            for (var j = 0; j < (length - i - 1); j++) {
                 if (items[j].point > items[j + 1].point) {
-                    //Swap the numbers
-                    var tmp = items[j];  //Temporary variable to hold the current number
-                    items[j] = items[j + 1]; //Replace current number with adjacent number
-                    items[j + 1] = tmp; //Replace adjacent number with current number
+                    var tmp = items[j];
+                    items[j] = items[j + 1];
+                    items[j + 1] = tmp;
                 }
             }
         }
